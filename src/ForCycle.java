b757e5c7ae25f968347a6585ef7dc63133821faa
kali <fless@inbox.ru>
2024-05-29 18:40:10 +0300

    Firs commit main

---
+++ src/ForCycle.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ForCycle {
+    public static void main(String[] args) {
+        System.out.print("Введите число для поиска пар: ");
+        int valueNumber = new Scanner(System.in).nextInt();
+
+        if (valueNumber < 0) {
+            System.out.println("Тут что - то отридцательное. Попробуйте ввести положительное число");
+        }
+
+        System.out.println("Как заказывали! Все пары положительных целых чисел!");
+
+        int countPairs = 0;
+
+        for (int i = 0; i <= valueNumber; i++) {
+            for (int j = 0; j <= valueNumber; j++) {
+                int multiplication = i * j;
+                if (multiplication == valueNumber) {
+                    System.out.println(i + " * " + j);
+                    countPairs++;
+                }
+            }
+
+        }
+        System.out.println("Всего у нас "+ countPairs + " пар(ы) положительных целых чисел" );
+    }
+
+
+}
+
+
+
